915584edb8342ad2c7aef79ab5fd3f4b869949c5
gefilte84 <58055602+gefilte84@users.noreply.github.com>
2020-09-01 14:29:40 +0200

    fullført program

--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,61 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-    state = { videos: [] };
+    // initierer state
+    state = { videos: [], selectedVideo: null };
+    
+    // gjør at det er noe default på siden
+    componentDidMount() {
+        this.onTermSubmit('destiny 2')
+    }
+    
+    
+    // søke funksjonen
     onTermSubmit = async term => {
        const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
-        this.setState ({ videos: response.data.items });
+        // resultat av søket
+        this.setState ({ 
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     };
     
+    // callback arrow funksjon
+    // dette må igjen refereres som props i render funksjonen
+    onVideoSelect = video => {
+        this.setState({ selectedVideo: video })
+    };
 
     render() {
         return (
+            // vi henter grid system fra semantic UI
             // container gjør at searchbar ikke strekker over hele siden (margin på sidene)
         <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit} />
-            <VideoList videos={this.state.videos} />
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                    <VideoDetail video={this.state.selectedVideo}/>
+                    </div>
+                    <div className="five wide column">
+                    <VideoList
+                    onVideoSelect={this.onVideoSelect} 
+                    videos={this.state.videos} 
+                    /></div>
+                    
+                </div>
+             </div>
         </div>
         );
     }
 }
 
 export default App;
+
