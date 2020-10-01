cd7125dc89f1cc9fde4ae40ab650552497bd7a36
gefilte84 <58055602+gefilte84@users.noreply.github.com>
2020-10-01 13:52:53 +0200

    lagt til hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
-class App extends React.Component {
-    // initierer state
-    state = { videos: [], selectedVideo: null };
-    
-    // gjør at det er noe default på siden
-    componentDidMount() {
-        this.onTermSubmit('destiny 2')
-    }
-    
-    
-    // søke funksjonen
-    onTermSubmit = async term => {
-       const response = await youtube.get('/search', {
-            params: {
-                q: term
-            }
-        });
-        // resultat av søket
-        this.setState ({ 
-            videos: response.data.items,
-            selectedVideo: response.data.items[0]
-        });
-    };
-    
-    // callback arrow funksjon
-    // dette må igjen refereres som props i render funksjonen
-    onVideoSelect = video => {
-        this.setState({ selectedVideo: video })
-    };
+//Lagt til hooks
 
-    render() {
-        return (
-            // vi henter grid system fra semantic UI
-            // container gjør at searchbar ikke strekker over hele siden (margin på sidene)
-        <div className="ui container">
-            <SearchBar onFormSubmit={this.onTermSubmit} />
-            <div className="ui grid">
-                <div className="ui row">
-                    <div className="eleven wide column">
-                    <VideoDetail video={this.state.selectedVideo}/>
-                    </div>
-                    <div className="five wide column">
-                    <VideoList
-                    onVideoSelect={this.onVideoSelect} 
-                    videos={this.state.videos} 
-                    /></div>
-                    
+const App = () => {
+    const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+    // gjør at det er noe default på siden
+    useEffect(() => {
+        onTermSubmit('destiny 2')
+    }, []);
+
+    // søke funksjonen
+    const onTermSubmit = async term => {
+        const response = await youtube.get('/search', {
+             params: {
+                 q: term
+             }
+         });
+         // resultat av søket
+         setVideos(response.data.items);
+         setSelectedVideo(response.data.items[0])
+         
+     };
+         // callback arrow funksjon
+    // dette må igjen refereres som props i render funksjonen
+    const onVideoSelect = video => {
+        setSelectedVideo(video);
+    };
+    return (
+        // vi henter grid system fra semantic UI
+        // container gjør at searchbar ikke strekker over hele siden (margin på sidene)
+    <div className="ui container">
+        <SearchBar onFormSubmit={onTermSubmit} />
+        <div className="ui grid">
+            <div className="ui row">
+                <div className="eleven wide column">
+                <VideoDetail video={selectedVideo}/>
                 </div>
-             </div>
-        </div>
-        );
-    }
-}
+                <div className="five wide column">
+                <VideoList
+                onVideoSelect={onVideoSelect} 
+                videos={videos} 
+                /></div>
+                
+            </div>
+         </div>
+    </div>
+    );
+};
 
 export default App;
 
